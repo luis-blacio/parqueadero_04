c8e6b731e9f84c0c9dfebf17ca6ff488c6a5ecdc
luis-blacio <luis.blacio@unl.edu.ec>
2024-11-25 01:37:23 -0500

    subclase automovil

---
+++ automovil.java
@@ -0,0 +1,2 @@
+public class automovil {
+}
--- vehiculo.java
+++ vehiculo.java
@@ -1,2 +1,26 @@
-public class vehiculo {
+public abstract class Vehiculo {
+    private String matricula;
+    private String modelo;
+    private String color;
+
+    public Vehiculo(String matricula, String modelo, String color) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.color = color;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public static class automovil {
+    }
 }
