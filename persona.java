d4f43ee824eff7a34d2ef340a1e7078252ff4be5
luis-blacio <luis.blacio@unl.edu.ec>
2024-11-25 01:26:10 -0500

    Clase Persona

--- persona.java
+++ persona.java
@@ -1,2 +1,24 @@
-public class persona {
+public abstract class Persona {
+    private String nombre;
+    private String apellido;
+    private String identificacion;
+
+    public Persona(String nombre, String apellido, String identificacion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
 }
+
