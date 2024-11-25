a6674935767959445d9c5fc712ff720649bebb7f
luis-blacio <luis.blacio@unl.edu.ec>
2024-11-25 01:46:08 -0500

    creacion de estacionamiento

--- estacionamiento.java
+++ estacionamiento.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+public class PlazaEstacionamiento {
+    private final int numero;
+    private boolean ocupada;
+    private Vehiculo vehiculo;
 
-public class estacionamiento {
+    public PlazaEstacionamiento(int numero) {
+        this.numero = numero;
+        this.ocupada = false;
+        this.vehiculo = null;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void ocupar(Vehiculo vehiculo) {
+        if (!ocupada) {
+            this.vehiculo = vehiculo;
+            ocupada = true;
+        } else {
+            System.out.println("Plaza ya ocupada.");
+        }
+    }
+
+    public void liberar() {
+        if (ocupada) {
+            vehiculo = null;
+            ocupada = false;
+        } else {
+            System.out.println("Plaza ya libre.");
+        }
+    }
 }
