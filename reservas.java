38aa427f5e0dfe6d61eb91af469c3fe5417d6a16
luis-blacio <luis.blacio@unl.edu.ec>
2024-11-25 01:52:42 -0500

    gitflow-feature-stash: parqueadero

--- reservas.java
+++ reservas.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+public class Reserva {
+    private Cliente cliente;
+    private PlazaEstacionamiento plaza;
+    private String fecha;
 
-public class reservas {
+    public Reserva(Cliente cliente, PlazaEstacionamiento plaza, String fecha) {
+        this.cliente = cliente;
+        this.plaza = plaza;
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public PlazaEstacionamiento getPlaza() {
+        return plaza;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void confirmarReserva() {
+        plaza.ocupar(null);
+    }
 }
