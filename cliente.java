a930e5160d77bc8b83ce225f0fd16a9825add5ad
luis-blacio <luis.blacio@unl.edu.ec>
2024-11-25 01:32:52 -0500

    clase empleado

--- cliente.java
+++ cliente.java
@@ -1,2 +1,18 @@
-public class cliente {
+public class Cliente extends Persona {
+    private boolean esRegular;
+    private String metodoPago;
+
+    public Cliente(String nombre, String apellido, String identificacion, boolean esRegular, String metodoPago) {
+        super(nombre, apellido, identificacion);
+        this.esRegular = esRegular;
+        this.metodoPago = metodoPago;
+    }
+
+    public boolean isEsRegular() {
+        return esRegular;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
 }
--- empleado.java
+++ empleado.java
@@ -1,2 +1,12 @@
-public class empleado {
+public class Empleado extends Persona {
+    private String cargo;
+
+    public Empleado(String nombre, String apellido, String identificacion, String cargo) {
+        super(nombre, apellido, identificacion);
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
 }
